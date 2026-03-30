b52e30824cd5d54097cce97ef2e16f8c0d88792b
Ryan Driscoll <driscollrp@gmail.com>
2026-03-30 14:01:53 -0600

    feat: add exam items dynamodb table

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
+  examItemsTable: dynamodb.Table;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.initializeTable();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  }
+
+  initializeTable() {
+    this.examItemsTable = new dynamodb.Table(this, 'ExamItems', {
+      tableName: 'ExamItems',
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    this.examItemsTable.addGlobalSecondaryIndex({
+      indexName: 'SubjectIndex',
+      partitionKey: { name: 'subjectKey', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'subjectSortKey', type: dynamodb.AttributeType.STRING },
+    });
+
+    this.examItemsTable.addGlobalSecondaryIndex({
+      indexName: 'StatusIndex',
+      partitionKey: { name: 'statusKey', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'statusSortKey', type: dynamodb.AttributeType.NUMBER },
+    });
+  }
   }
 }
