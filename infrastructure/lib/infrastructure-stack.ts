9e9947edf2cfb5f5614d8978f796f8156d4d8d09
Ryan Driscoll <driscollrp@gmail.com>
2026-03-30 14:02:46 -0600

    feat: add lambda functions

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,15 +1,21 @@
 import * as cdk from 'aws-cdk-lib/core';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   examItemsTable: dynamodb.Table;
+  createItemHandler: lambda.Function;
+  getItemHandler: lambda.Function;
+  updateItemHandler: lambda.Function;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     this.initializeTable();
 
+    this.initializeFunctions();
   }
 
   initializeTable() {
@@ -31,6 +37,47 @@ export class InfrastructureStack extends cdk.Stack {
       partitionKey: { name: 'statusKey', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'statusSortKey', type: dynamodb.AttributeType.NUMBER },
     });
+  }
+
+  initializeFunctions() {
+    this.createItemHandler = new lambda.Function(this, 'CreateItemHandler', {
+      functionName: 'CreateItemHandler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'handlers/items.createItemHandler',
+      code: lambda.Code.fromAsset('../dist'),
+      environment: {
+        DYNAMODB_TABLE_NAME: this.examItemsTable.tableName,
+        USE_DYNAMODB: 'true',
+      },
+    });
+
+    this.getItemHandler = new lambda.Function(this, 'GetItemHandler', {
+      functionName: 'GetItemHandler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'handlers/items.getItemHandler',
+      code: lambda.Code.fromAsset('../dist'),
+      environment: {
+        DYNAMODB_TABLE_NAME: this.examItemsTable.tableName,
+        USE_DYNAMODB: 'true',
+      },
+    });
+
+    this.updateItemHandler = new lambda.Function(this, 'UpdateItemHandler', {
+      functionName: 'UpdateItemHandler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: 'handlers/items.updateItemHandler',
+      code: lambda.Code.fromAsset('../dist'),
+      environment: {
+        DYNAMODB_TABLE_NAME: this.examItemsTable.tableName,
+        USE_DYNAMODB: 'true',
+      },
+    });
+
+    this.examItemsTable.grantReadWriteData(this.createItemHandler);
+
+    this.examItemsTable.grantReadData(this.getItemHandler);
+
+    this.examItemsTable.grantReadWriteData(this.updateItemHandler);
   }
   }
 }
